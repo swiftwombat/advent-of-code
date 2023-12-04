72023fe2ad35304d18ea3d1e18971b5aecdd552c
SwiftWombat <zcockshutt@gmail.com>
2023-12-04 14:29:17 +1100

    Refactoring day 2

--- 2023/src/Day02.java
+++ 2023/src/Day02.java
@@ -1,7 +1,9 @@
+import static java.lang.Math.max;
+
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Zachary Cockshutt
@@ -9,13 +11,8 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class Day02 extends Day
 {
-    
-    private final static Map<String, Integer> maxCubeCounts = new HashMap<>()
-    {{
-        put("red",  12);
-        put("green",13);
-        put("blue", 14);
-    }};
+    private static final int MAX_RED = 12, MAX_GREEN = 13, MAX_BLUE = 14;
+    private static final Pattern[] rgbRgx = { Pattern.compile("(\\d+) red"), Pattern.compile("(\\d+) green"), Pattern.compile("(\\d+) blue") };
 
     @Override
     public String partOne() throws IOException
@@ -23,15 +20,14 @@ public class Day02 extends Day
         var sum = new AtomicInteger(0);
         this.streamInput((s) ->
         {
-            var arr = s.split("[ :;,]");
-            var isPossible = true;
-            for (int i = 3; i < arr.length-1; i+=3)
+            var flag = 1;
+            for (var set : s.split(";"))
             {
-                var count = Integer.parseInt(arr[i]);
-                isPossible = count <= maxCubeCounts.get(arr[i+1]);
-                if (!isPossible) { break; }
+                var rgb = processSet(set);
+                if (!rgb.isPossible()) { flag = 0; break; }
             }
-            if (isPossible) { sum.set(sum.get() + Integer.parseInt(arr[1])); }
+            var id = Integer.parseInt(s.split(":")[0].split(" ")[1]) * flag;
+            sum.set(sum.get() + id);
         });
         return sum.toString();
     }
@@ -42,17 +38,39 @@ public class Day02 extends Day
         var sum = new AtomicInteger(0);
         this.streamInput((s) -> 
         {
-            var cubeCounts = new HashMap<String, Integer>() {{ put("red", 0); put("green", 0); put("blue", 0); }};
-            var arr = s.split("[ :;,]");
-            for (int i = 3; i < arr.length-1; i+=3)
+            int r = 0, g = 0, b = 0;
+            for (var set : s.split(";"))
             {
-                var count = Integer.parseInt(arr[i]);
-                var colour = arr[i+1];
-                if (cubeCounts.get(colour) < count) { cubeCounts.put(colour, count); }
+                var rgb = processSet(set);
+                r = max(r, rgb.red);
+                g = max(g, rgb.green);
+                b = max(b, rgb.blue);
             }
-            var power = cubeCounts.get("red") * cubeCounts.get("green") * cubeCounts.get("blue");
+            var power = r * b * g;
             sum.set(sum.get() + power);
         });
         return sum.toString();
+    }
+
+    private Set processSet(String set)
+    {
+        var rgb = new int[3];
+        for (int i=0; i<3; i++) { rgb[i] = this.getColourCount(rgbRgx[i], set); }
+        return new Set(rgb[0], rgb[1], rgb[2]);
+    }
+    
+    private int getColourCount(Pattern p, String str)
+    {
+        final Matcher matcher = p.matcher( str );
+        return matcher.find() ? Integer.parseInt(matcher.group(1).split(" ")[0]) : 0;
+    }
+
+    private record Set(int red, int green, int blue) 
+    {
+        private boolean isPossible()
+        {
+            var isPossible = red <= MAX_RED && green <= MAX_GREEN && blue <= MAX_BLUE;
+            return isPossible;
+        }
     }
 }
