54fe2061e0c690d24643a3ccbdafb3ecc0ddad01
SwiftWombat <zcockshutt@gmail.com>
2023-12-06 01:24:04 +1100

    Parsing input for day 5

--- 2023/src/Day05.java
+++ 2023/src/Day05.java
@@ -1,5 +1,7 @@
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
 
 /**
  * @author Zachary Cockshutt
@@ -11,10 +13,7 @@ public class Day05 extends Day
     public String partOne() throws IOException
     {
         var sum = new AtomicInteger(0);
-        this.input((s) -> 
-        {
-            
-        });
+        var almanac = this.parseAlmanac();
         return sum.toString();
     }
 
@@ -22,10 +21,39 @@ public class Day05 extends Day
     public String partTwo() throws IOException
     {
         var sum = new AtomicInteger(0);
-        this.input((s) -> 
-        {
-            
-        });
+        var almanac = this.parseAlmanac();
         return sum.toString();
     }
+
+    private Almanac parseAlmanac() throws IOException
+    {
+        var input = this.input();
+        var n = input.length;
+        var seeds = Stream.of(input[0].substring(7).split(" +")).mapToLong(Long::parseLong).toArray();
+        var categories = new ArrayList<AlmanacMap[]>();
+        for (int i = 2; i < n; i++)
+        {
+            if (isAlpha(input[i].charAt(0))) { continue; }
+            var maps = new ArrayList<AlmanacMap>();
+            while (i < n && !input[i].isEmpty()) { maps.add(this.parseMap(input[i++])); }
+            categories.add(maps.toArray(new AlmanacMap[0]));
+        }
+        return new Almanac(seeds, categories.toArray(new AlmanacMap[0][]));
+    }
+
+    private AlmanacMap parseMap(String s)
+    {
+        var arr = Stream.of(s.split(" ")).mapToLong(Long::parseLong).toArray();
+        return new AlmanacMap(arr[0], arr[1], arr[2]);
+    }
+
+    private boolean isAlpha(char ch)
+    {
+        return ch >= 'a'
+            && ch <= 'z';
+    }
+
+    private record Almanac(long[] seeds, AlmanacMap[][] categories) {}
+
+    private record AlmanacMap(long dest, long source, long range) {}
 }
