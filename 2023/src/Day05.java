8c46a011f9f862410b33ad19b1eedcf8884dd0c7
swiftwombat <61484083+swiftwombat@users.noreply.github.com>
2023-12-06 13:39:29 +1100

    Reducing code resuse for day 5

--- 2023/src/Day05.java
+++ 2023/src/Day05.java
@@ -1,6 +1,7 @@
+import static java.lang.Math.min;
+
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
 /**
@@ -12,24 +13,24 @@ public class Day05 extends Day
     @Override
     public String partOne() throws IOException
     {
-        var sum = new AtomicInteger(0);
-        var almanac = this.parseAlmanac();
-        return sum.toString();
+        var almanac = this.parseAlmanac(false);
+        var location = almanac.getMinLocation();
+        return String.valueOf(location);
     }
 
     @Override
     public String partTwo() throws IOException
     {
-        var sum = new AtomicInteger(0);
-        var almanac = this.parseAlmanac();
-        return sum.toString();
+        var almanac = this.parseAlmanac(true);
+        var location = almanac.getMinLocation();
+        return String.valueOf(location);
     }
 
-    private Almanac parseAlmanac() throws IOException
+    private Almanac parseAlmanac(boolean isPartTwo) throws IOException
     {
         var input = this.input();
         var n = input.length;
-        var seeds = Stream.of(input[0].substring(7).split(" +")).mapToLong(Long::parseLong).toArray();
+        var seeds = isPartTwo ? parseSeedsAsRanges(input[0]) : parseSeeds(input[0]);
         var categories = new ArrayList<AlmanacMap[]>();
         for (int i = 2; i < n; i++)
         {
@@ -39,6 +40,21 @@ public class Day05 extends Day
             categories.add(maps.toArray(new AlmanacMap[0]));
         }
         return new Almanac(seeds, categories.toArray(new AlmanacMap[0][]));
+    }
+
+    private Seed[] parseSeeds(String s)
+    {
+        return Stream.of(s.substring(7).split(" +"))
+            .map(x->new Seed(Long.parseLong(x), Long.parseLong(x)))
+            .toArray(Seed[]::new);
+    }
+    
+    private Seed[] parseSeedsAsRanges(String s)
+    {
+        var seeds = new ArrayList<Seed>();
+        var numbers = Stream.of(s.substring(7).split(" +")).mapToLong(Long::parseLong).toArray();
+        for (int i = 0; i < numbers.length; i+=2) { seeds.add(new Seed(numbers[i], (numbers[i]+numbers[i+1]))); }
+        return seeds.stream().toArray(Seed[]::new);
     }
 
     private AlmanacMap parseMap(String s)
@@ -53,7 +69,30 @@ public class Day05 extends Day
             && ch <= 'z';
     }
 
-    private record Almanac(long[] seeds, AlmanacMap[][] categories) {}
+    private record Almanac(Seed[] seeds, AlmanacMap[][] categories) 
+    {
+        private long getMinLocation()
+        {
+            var location = Long.MAX_VALUE;
+            for (var seed : seeds)
+                for (long i = seed.start(); i <= seed.end(); i++)
+                    location = min(location, this.getLocation(i));
+            return location;
+        }
+
+        private long getLocation(long seed)
+        {
+            for (var category : this.categories)
+                for (var map : category)
+                {
+                    boolean isMapped = seed >= map.source && seed < map.source + map.range();
+                    if (isMapped) { seed = map.dest + (seed - map.source); break; }
+                }
+            return seed;
+        }
+    }
 
     private record AlmanacMap(long dest, long source, long range) {}
+
+    private record Seed(long start, long end) {}
 }
