602876e318dbfccb76a483fd1d4ff6cdc8f58d33
SwiftWombat <zcockshutt@gmail.com>
2023-12-08 20:02:21 +1100

    Adding day 7 part 1

--- 2023/src/Day07.java
+++ 2023/src/Day07.java
@@ -1,8 +1,7 @@
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Comparator;
+import java.util.HashMap;
 
 /**
  * @author Zachary Cockshutt
@@ -10,28 +9,24 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class Day07 extends Day
 {
+    private final static String RANKS = "AKQJT98765432";
+
     @Override
     public String partOne() throws IOException
     {
-        var sum = new AtomicInteger(0);
+        int sum = 0;
         var hands = new ArrayList<Hand>();
-        this.input((s) -> 
-        {
-            var hand = parseHand(s);
-            System.out.println();
-        });
-        return sum.toString();
+        this.input((s) -> hands.add(parseHand(s)));
+        hands.sort(new HandComparator());
+        for (int i = 0; i < hands.size(); i++) { sum += hands.get(i).bid * (i+1); }
+        return String.valueOf(sum);
     }
 
     @Override
     public String partTwo() throws IOException
     {
-        var sum = new AtomicInteger(0);
-        this.input((s) -> 
-        {
-            
-        });
-        return sum.toString();
+        int sum = 0;
+        return String.valueOf(sum);
     }
 
     private Hand parseHand(String s)
@@ -45,10 +40,34 @@ public class Day07 extends Day
 
     private int parseHandType(char[] cards)
     {
-        var s = new HashSet<Character>();
-        for(int i = 0; i < cards.length; i++) { s.add(cards[i]); }
-        return s.size();
+        var max = 0;
+        var map = new HashMap<Character, Integer>();
+        for (var c : cards) 
+        { 
+            var i = map.containsKey(c) ? map.get(c)+1 : 1;
+            if (i > max) { max = i; };
+            map.put(c, i); 
+        }
+        return map.size() - max;
     }
 
     private record Hand(char[] cards, int bid, int type) {}
+
+    private record HandComparator() implements Comparator<Hand>
+    {
+        @Override
+        public int compare(Hand a, Hand b)
+        {
+            if (a.type < b.type) { return 1; }
+            if (a.type > b.type) { return -1; }
+            for (int i = 0; i < 5; i++) 
+            {
+                var c = RANKS.indexOf(a.cards[i]);
+                var o = RANKS.indexOf(b.cards[i]);
+                if (c == o) { continue; }
+                return c < o ? 1 : -1;
+            }
+            return 0;
+        }
+    }
 }
