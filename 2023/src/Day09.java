381a99d68a0797dffc05a6a1a1f59c81264ae4c8
SwiftWombat <zcockshutt@gmail.com>
2023-12-09 17:00:39 +1100

    Adding day 9 base

---
+++ 2023/src/Day09.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+
+/**
+ * @author Zachary Cockshutt
+ * @since  2023-12-09
+ */
+public class Day09 extends Day
+{
+    @Override
+    public String partOne() throws IOException
+    {
+        int sum = 0;
+        return String.valueOf(sum);
+    }
+
+    @Override
+    public String partTwo() throws IOException
+    {
+        int sum = 0;
+        return String.valueOf(sum);
+    }
+}
