348edcc256f01332bbd6cc9e8c51ad18cd4c4e86
SwiftWombat <zcockshutt@gmail.com>
2023-12-13 20:24:09 +1100

    Adding day 12 base

---
+++ 2023/src/Day12.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * @author Zachary Cockshutt
+ * @since  2023-12-13
+ */
+public class Day12 extends Day
+{
+    @Override
+    public String partOne() throws IOException
+    {
+        var sum = new AtomicLong(0L);
+        this.input((s) -> 
+        {
+
+        });
+        return sum.toString();
+    }
+
+    @Override
+    public String partTwo() throws IOException
+    {
+        var sum = new AtomicLong(0L);
+        this.input((s) -> 
+        {
+
+        });
+        return sum.toString();
+    }
+}
