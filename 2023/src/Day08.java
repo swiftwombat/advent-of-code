c273a6a366e87fe8fed1c9cfd650cc3b5cd30240
SwiftWombat <zcockshutt@gmail.com>
2024-12-02 00:17:38 +1100

    Fixing 2023 Day 8 to actually print solution

--- 2023/src/Day08.java
+++ 2023/src/Day08.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
@@ -31,29 +32,58 @@ public class Day08 extends Day
         return String.valueOf(count);
     }
 
-    // 11653, 21409, 19241, 12737, 14363, 15989
     @Override
     public String partTwo() throws IOException
     {
-        int count = 0;
         var input = this.input();
         var dirs = input[0].toCharArray();
         var nodes = new HashMap<String, Node>();
+        var starts = new ArrayList<String>();
         for (int i = 2; i < input.length; i++)
         {
             var codes = input[i].replace(")","").split(" = \\(|, ");
             var node = new Node(codes[1], codes[2]);
             nodes.put(codes[0], node);
+            if (codes[0].charAt(2) == 'A') { starts.add(codes[0]); }
         }
-        var curr = "PSA";
-        while (curr.charAt(2) != 'Z')
+        var counts = new int[starts.size()];
+        for (int i = 0; i < counts.length; i++)
         {
-            int i = Math.floorMod(count++, dirs.length);
-            var node = nodes.get(curr);
-            var dir = dirs[i];
-            curr = dir == 'L' ? node.left : node.right;
+            var curr = starts.get(i);
+            while (curr.charAt(2) != 'Z')
+            {
+                int j = Math.floorMod(counts[i]++, dirs.length);
+                var node = nodes.get(curr);
+                var dir = dirs[j];
+                curr = dir == 'L' ? node.left : node.right;
+            }
         }
-        return String.valueOf(count);
+        return String.valueOf(lcm(counts));
+    }
+
+    public static long lcm(int[] arr)
+    {
+        long lcm = 1;
+        int divisor = 2;
+        while (true) 
+        {
+            int count = 0;
+            boolean divisible = false;
+            for (int i = 0; i < arr.length; i++) 
+            {
+                if (arr[i] == 0)     { return 0; }
+                else if (arr[i] < 0) { arr[i] = arr[i] * (-1); }
+                if (arr[i] == 1) { count++; }
+                if (arr[i] % divisor == 0) 
+                {
+                    divisible = true;
+                    arr[i] = arr[i] / divisor;
+                }
+            }
+            if (divisible) { lcm *= divisor; }
+            else           { divisor++;  }
+            if (count == arr.length) { return lcm; }
+        }
     }
 
     private record Node(String left, String right) {}
