d3cdcd04dfeb3a947174b9d8ca61947f8f8b23bc
SwiftWombat <zcockshutt@gmail.com>
2024-12-03 20:12:14 +1100

    Adding day 3 solution

---
+++ 2024/src/Day03.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Text.RegularExpressions;
+
+/**
+ * @author Zachary Cockshutt
+ * @since  2024-12-03
+ */
+public class Day03 : Day
+{
+    public override string PartOne()
+    {
+        long total = 0L;
+        string memory = string.Join("", this.Input());
+        string pattern = @"mul\((?<a>\d+),(?<b>\d+)\)";
+        var matches = Regex.Matches(memory, pattern);
+        foreach (Match m in matches) { total += Multiply(m); }
+        return total.ToString();
+    }
+
+    public override string PartTwo()
+    {
+        long total = 0L;
+        bool enabled = true;
+        string memory = string.Join("", this.Input());
+        string pattern = @"mul\((?<a>\d+),(?<b>\d+)\)|do\(\)|don't\(\)";
+        var matches = Regex.Matches(memory, pattern);
+        foreach (Match m in matches)
+        {
+            string val = m.Value;
+            if (!val.StartsWith("mul")) { enabled = val.Equals("do()"); }
+            else if (enabled)           { total += Multiply(m); }
+        }
+        return total.ToString();
+    }
+
+    private static int Multiply(Match match)
+    {
+        int a = int.Parse(match.Groups["a"].Value);
+        int b = int.Parse(match.Groups["b"].Value);
+        return  a * b;
+    }
+}
